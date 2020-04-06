6bd9dfed98c18be81076a0b36d6f24808d627bdf
Samantha Garcia <sgarcia22@ufl.edu>
2020-04-06 19:05:16 -0400

    Initial commit

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    public Material grey, pink;
+
+    private MeshRenderer rend;
+    private XRGrabInteractable grabInteractable;
+
+    void Awake()
+    {
+        rend = GetComponent<MeshRenderer>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        grabInteractable.onActivate.AddListener(SetPink);
+        grabInteractable.onDeactivate.AddListener(SetGrey);
+    }
+
+    private void SetPink(XRBaseInteractor interactor)
+    {
+        rend.material = pink;
+    }
+
+    private void SetGrey(XRBaseInteractor interactor)
+    {
+        rend.material = grey;
+    }
+
+    private void OnDestroy()
+    {
+        grabInteractable.onActivate.RemoveListener(SetPink);
+        grabInteractable.onDeactivate.RemoveListener(SetGrey);
+    }
+}
